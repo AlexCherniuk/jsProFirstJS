80972c14c53cfc9110a098ec7bfeee63e9516dcc
OleksandCherniuk <alexandr24021990@gmail.com>
2024-02-02 18:50:36 +0100

    firstLesson

---
+++ script.js
@@ -0,0 +1,5 @@
+// let text = ";alskjdfkasjdfjaklsjd;fja;sljdf;ajsdjfl;asdjflaskjdfl;kasjblvj nclxjzg.jdf,f";
+// const numbers = 123424343;
+alert("text");
+let text = "sldjfa;ldj;lafs" //"" - type of data = text
+window.alert("our first js lesson")
