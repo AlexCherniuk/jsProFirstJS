3d16338f6eef63354740ba7c4e02c8aff97a3368
OleksandCherniuk <alexandr24021990@gmail.com>
2024-02-09 18:44:27 +0100

    types

--- script.js
+++ script.js
@@ -47,16 +47,118 @@
 //     2: true
 // };
 
-let storeName = 'atb';
+// let storeName = 'atb';
 
-const storeDescriprion  = {
-    budget: 10000,
-    employees: ['Maria', 'Theo', 'Alex'],
-    productsPrise: {
-        pen: 30, 
-        note: 15
-    },
-    isOpen: true
-};
+// const storeDescriprion  = {
+//     budget: 10000,
+//     employees: ['Maria', 'Theo', 'Alex'],
+//     productsPrise: {
+//         pen: 30, 
+//         note: 15
+//     },
+//     isOpen: true
+// };
 
-console.log(storeDescriprion.employees[1])
+// console.log(storeDescriprion.employees[1])
+
+
+// const user = {};
+// user.name = "Natalia";
+// user.age = 33;
+// user.isMarried = true;
+// user.favColors = ["red", "blue", "white"];
+// user.data = {
+//     city: "Kyiv",
+//     likeAnimals: true
+// };
+
+// console.log(user);
+
+// const project = {
+//     name: "Real Estate",
+//     price: 3000,
+//     open: true,
+//     employees: ["Santa", "Maria", "Lucia"],
+//     apartments: {
+//         oneRoomSquare: 60,
+//         twoRoomSquare: 100
+//     }
+// };
+
+
+// const stockholm = {
+//     budget: 1500000,
+//     mainStreet: 'Vasagatan',
+//     isBiggestCity: true,
+//     bestHotels: {
+//         scandic: 100,
+//         blueLaguna: 150,
+//         elite: 250,
+//     }
+// };
+
+// const object = {
+//     text: 'hello',
+//     number: 5,
+//     question: true, 
+//     ojectThree: {
+//         name: 'Maria',
+//         age: 18,
+//         isChildren: false
+//     },
+//     arr: ['blue', 'white', 'yellow', 121, false, {text: 'HelloWorld'}, [1, 3, 4, 5]]
+// };
+
+// let arr = ['blue', 'white', 'yellow', 121, false, {text: 'HelloWorld'}, [1, 3, 4, 5]]; //array
+
+// let name1;
+// let data = [1, "black", true, null, name1]; //array
+
+
+// const result = confirm("Are you here?");
+
+// console.log(result)
+
+// const answer = prompt("Скільки вам років?", "")
+// console.log(typeof(answer + 5));
+
+// const answers = [];
+// answers[0] = prompt("Your Name?", "");
+// answers[1] = prompt("Your Age?", "");
+// answers[2] = prompt("Your weight?", "");
+
+// // console.log(answers);
+// document.write(answers);
+
+
+// const category = 'toys';
+
+// console.log('http://someurl.com/' + category + '/' + '4' );
+
+// console.log(`http://someurl.com/${category}/6/hello`);// інтерполяція
+
+
+// const user = "Nataliia";
+
+// alert(`Hello ${user}`);
+
+// + - : /
+
+
+
+// console.log(2 + "px");  //"24" 
+
+// let number = 6;
+// // increment  ++
+// // decrement --
+// console.log(number++); // 6
+// console.log(number); //  7
+// console.log(number--); // 
+// console.log(++number); // 
+
+
+// console.log(2 + 2 === "4")
+
+console.log(false == "jgjjgjj") //falsy  - 0 , false, "", null, undefined, Nan - 6шт
+
+// n = n + 1 // n = n++
